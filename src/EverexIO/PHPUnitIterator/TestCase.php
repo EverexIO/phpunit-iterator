6e024e43b1dca7c8b477624a45dc45b3365575a7
Artem <artemk@211.ru>
2017-11-15 14:36:00 +0700

    Composer files and empty class added

---
+++ src/EverexIO/PHPUnitIterator/TestCase.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace EverexIO\PHPUnitIterator;
+
+use PHPUnit_Framework_TestCase;
+use ReflectionClass;
+use ReflectionMethod;
+use ReflectionProperty;
+
+/**
+ *
+ */
+class TestCase extends PHPUnit_Framework_TestCase
+{
+}
