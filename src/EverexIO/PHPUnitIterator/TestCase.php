7e92fa669482e1c927f2c7fae74aed9395f93323
Artem <artemk@211.ru>
2017-11-15 15:38:03 +0700

    Use namespaces in inheritance

--- src/EverexIO/PHPUnitIterator/TestCase.php
+++ src/EverexIO/PHPUnitIterator/TestCase.php
@@ -2,14 +2,9 @@
 
 namespace EverexIO\PHPUnitIterator;
 
-use PHPUnit_Framework_TestCase;
-use ReflectionClass;
-use ReflectionMethod;
-use ReflectionProperty;
-
 /**
  *
  */
-class TestCase extends PHPUnit_Framework_TestCase
+class TestCase extends \PHPUnit\Framework\TestCase
 {
 }
