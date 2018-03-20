0709a363982560543ca6b6347da740c08806f690
Artem <artemk@211.ru>
2018-03-20 17:13:16 +0700

    Classname fix

--- src/EverexIO/PHPUnitIterator/TestCaseBase.php
+++ src/EverexIO/PHPUnitIterator/TestCaseBase.php
@@ -5,8 +5,8 @@ namespace EverexIO\PHPUnitIterator;
 if (class_exists('\PHPUnit\Framework\TestCase')) {
     class TestCaseBase extends \PHPUnit\Framework\TestCase {}
 } 
-else if (class_exists('PHPUnit_Framework_TestCase')) {
-    class TestCaseBase extends PHPUnit_Framework_TestCase {}
+else if (class_exists('\PHPUnit_Framework_TestCase')) {
+    class TestCaseBase extends \PHPUnit_Framework_TestCase {}
 }
 else {
     die('No suitable TestCase class found, please check PHPUnit version!');
